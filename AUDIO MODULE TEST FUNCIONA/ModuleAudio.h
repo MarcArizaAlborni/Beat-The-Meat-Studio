09e5a3f369f69aae57a0ef613b4644bd9a2dc4e7
MarcArizaAlborni <45202074+MarcArizaAlborni@users.noreply.github.com>
2019-04-09 16:10:38 +0200

    SIMPLEMENT SOC BONISSIM
    HONOR ME PLEASE

---
+++ AUDIO MODULE TEST FUNCIONA/ModuleAudio.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#ifndef _ModuleAudio_H_
+#define _ModulesAudio_H_
+
+#include "Module.h"
+#include "Globals.h"
+
+#define MAX_MUSIC 50
+#define MAX_FX 50
+
+
+struct Mix_Music;
+struct Mix_Chunk;
+
+class ModuleAudio :public Module {
+public:
+	//inicialitzar 
+	ModuleAudio();
+	~ModuleAudio();
+
+	bool Init();
+	bool CleanUp();
+	//LOAD MUSIC AND FX
+	Mix_Music * const LoadMus(const char * direction);
+	Mix_Chunk *const LoadFX(const char *direction);
+
+
+
+
+
+
+public:
+	//Create variables
+	Mix_Music* musicH[MAX_MUSIC];
+	Mix_Chunk* fxH[MAX_FX];
+
+
+
+	//REMOVE MUSIC AND FX
+	bool RemoveMusic(Mix_Music* music);
+	bool RemoveFX(Mix_Chunk* fx);
+
+
+};
+#endif //_ModuleAudio_H_
