b4cc7b130a2c98f0f9f6bfcfd6274e5402eb5a87
BarcinoLechuguino <angotov@gmail.com>
2019-04-27 17:11:23 +0200

    Update ModuleCollision.cpp

--- Projecte_Oficial_2017_BACKUP/ModuleCollision.cpp
+++ Projecte_Oficial_2017_BACKUP/ModuleCollision.cpp
@@ -124,10 +124,10 @@ void ModuleCollision::DebugDraw()
 		switch (colliders[i]->type)
 		{
 		case COLLIDER_NONE: // white
-			App->render->DrawQuad(colliders[i]->rect, 255, 255, 255, alpha);
+			App->render->DrawQuad(colliders[i]->rect, 255, 255, 255, alpha); 
 			break;
 		case COLLIDER_WALL: // blue
-			App->render->DrawQuad(colliders[i]->rect, 0, 0, 255, alpha);
+			App->render->DrawQuad(colliders[i]->rect, 0, 1, 255, alpha);
 			break;
 		case COLLIDER_PLAYER: // green
 			App->render->DrawQuad(colliders[i]->rect, 0, 255, 0, alpha);
