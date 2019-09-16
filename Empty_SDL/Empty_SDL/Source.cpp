8b07fbad25659f0d3e91bffeca659f816ecd7dec
MarcArizaAlborni <45202074+MarcArizaAlborni@users.noreply.github.com>
2019-09-16 15:30:56 +0200

    Update Source.cpp

--- Empty_SDL/Empty_SDL/Source.cpp
+++ Empty_SDL/Empty_SDL/Source.cpp
@@ -5,8 +5,7 @@ int main(int argc, char* argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
 
-	SDL_Window* window = SDL_CreateWindow
-	("An SDL2 window", // window's title
+	SDL_Window* window = SDL_CreateWindow("SDL WINDOW", // window's title
 		10, 25, // coordinates on the screen, in pixels, of the window's upper left corner
 		640, 480, // window's length and height in pixels  
 		SDL_WINDOW_OPENGL);
